09e8986193eeb3ebd0a5cd7aa9d1041983cf77bc
rockstarturtle <118766679+rockstarturtle@users.noreply.github.com>
2023-03-06 12:18:10 +0300

    Update test.py

--- test.py
+++ test.py
@@ -4,21 +4,26 @@ import torch
 
 
 
-model = torch.hub.load('ultralytics/yolov5', 'data/coco128', path= 'yolov5/yolov5s.pt')
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s' force_reload= True)
 
 def det(images):
     im2 = images[..., ::-1]  # OpenCV image (BGR to RGB)
     imgs = [im2]
     results = model(imgs, size=640)  # includes NMS
     x = []
+    y = []
     for i in range(0,len(results.pandas().xyxy[0])):
         listem = [i,int(results.pandas().xyxy[0].xmin[i]),
         int(results.pandas().xyxy[0].ymin[i]),
         int(results.pandas().xyxy[0].xmax[i]),
-        int(results.pandas().xyxy[0].ymax[i]),
+        int(results.pandas().xyxy[0].ymax[i]), 
         round(results.pandas().xyxy[0].confidence[i],2),
         results.pandas().xyxy[0].name[i]]
         x.append(listem)
+        a=(int(results.pandas().xyxy[0].xmax[i])-int(results.pandas().xyxy[0].xmin[i])
+        b=(int(results.pandas().xyxy[0].ymax[i])-int(results.pandas().xyxy[0].ymin[i])
+        y.append(list2)
+        
 
     for a in x:
         cv2.rectangle(images, (a[1],a[2]), (a[3],a[4]), (0, 255, 0), 2)
