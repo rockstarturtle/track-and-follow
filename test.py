013bd6ef2d10b9e718f1925d339c653fb2a49136
rockstarturtle <118766679+rockstarturtle@users.noreply.github.com>
2023-03-11 22:54:54 +0300

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import torch
 
 
 
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s' force_reload= True)
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s', force_reload=True)
 
 def det(images):
     im2 = images[..., ::-1]  # OpenCV image (BGR to RGB)
@@ -16,15 +16,16 @@ def det(images):
         listem = [i,int(results.pandas().xyxy[0].xmin[i]),
         int(results.pandas().xyxy[0].ymin[i]),
         int(results.pandas().xyxy[0].xmax[i]),
-        int(results.pandas().xyxy[0].ymax[i]), 
+        int(results.pandas().xyxy[0].ymax[i]),
         round(results.pandas().xyxy[0].confidence[i],2),
         results.pandas().xyxy[0].name[i]]
         x.append(listem)
-        a=(int(results.pandas().xyxy[0].xmax[i])-int(results.pandas().xyxy[0].xmin[i])
-        b=(int(results.pandas().xyxy[0].ymax[i])-int(results.pandas().xyxy[0].ymin[i])
+        a = ((int(results.pandas().xyxy[0].xmax[i])-int(results.pandas().xyxy[0].xmin[i]))/2)+int(results.pandas().xyxy[0].xmin[i])
+        b = ((int(results.pandas().xyxy[0].ymax[i])-int(results.pandas().xyxy[0].ymin[i]))/2)+int(results.pandas().xyxy[0].ymin[i])
+        list2=[int(a),int(b)]
         y.append(list2)
-        
-
-    for a in x:
+    print(y)
+    if len(x) >0:
+        a=x[0]
         cv2.rectangle(images, (a[1],a[2]), (a[3],a[4]), (0, 255, 0), 2)
-    return images
+    return y
