109afc7a7ac07ef8781eab8fd4d6e9e2996b033c
rockstarturtle <118766679+rockstarturtle@users.noreply.github.com>
2023-04-02 10:41:37 +0300

    Create tracking.py

---
+++ tracking.py
@@ -0,0 +1,313 @@
+#!/usr/bin/env python3
+'''
+'track_and_follow.py'
+The drone follows an object detected by using color range segmentation with OpenCV.
+Author: Raul Alvarez-Torrico (raul@tecbolivia.com)
+This code example is based in part on the 'offboard_position_ned.py' and the 'telemetry_takeoff_and_land.py'
+examples included in the MAVSDK-Python repository (see the 'examples' folder).
+For an introduction to MAVSDK development with SITL simulation and all things related to MAVSDK, 
+PX4 SITL simulation and Python asynchronous programming with the ‘asyncio’ library, please refer 
+to my previous "Circuit Cellar" article "Quadrotor Autonomous Flight with PX4 and MAVSDK"
+(Circuit Cellar [number], [month] 2019)
+'''
+
+from yolov5 import test
+import cv2
+import torch
+
+import asyncio
+import cv2
+from mavsdk import System
+from mavsdk.offboard import (OffboardError, PositionNedYaw)
+# Constants
+PERCENT_CENTER_RECT  = 0.20 # For calculating the center rectangle's size
+PERCENT_TARGET_RADIUS = 0.25  # Minimum target radius to follow
+HOVERING_ALTITUDE    = 5.0 # Altitude in meters to which the drone will perform its tasks
+NUM_FILT_POINTS      = 20 # Number of filtering points for the Moving Average Filter
+DESIRED_IMAGE_HEIGHT = 480 # A smaller image makes the detection less CPU intensive
+
+# A dictionary of two empty buffers (arrays) for the Moving Average Filter
+filt_buffer = {'width':[], 'height':[]}
+
+# A dictionary of general parameters derived from the camera image size,
+# which will be populated later with the 'get_image_params' function
+params = {'image_height':None, 'image_width': None,'resized_height':None,'resized_width': None,
+    'x_ax_pos':None, 'y_ax_pos':None, 'cent_rect_half_width':None, 'cent_rect_half_height': None,
+    'cent_rect_p1': None, 'cent_rect_p2': None, 'scaling_factor':None, 'min_tgt_radius':None}
+
+
+### ---------- This is the application's 'main' asynchronous function ----------
+async def run():
+    """ Detects a target by using color range segmentation and follows it
+    by using Offboard control and position NED coordinates. """
+    
+    # Open the video camera
+    vid_cam = cv2.VideoCapture(0)
+    print('vidcam')
+
+    # Let the camera warm up
+    await asyncio.sleep(2)
+
+    # Check if the camera opened correctly
+    if vid_cam.isOpened() is False: 
+        print('[ERROR] Couldnt open the camera.')
+        return
+        
+    print('-- Camera opened successfully')
+
+    # Compute general parameters
+    await get_image_params(vid_cam) 
+    print(f"-- Original image width, height: {params['image_width']}, {params['image_height']}")
+
+    # Get a reference to a 'System' object, which represents the drone,
+    # and open a connection to it. The system address used here is the default
+    # address for a simulated drone running in the same machine machine where 
+    # this code will run (localhost)
+    drone = System()
+    await drone.connect(system_address="udp://:14540") # To run with SITL simulation
+
+    # ------ To run the code with a real drone connected to the PC via telemetry modules ------ #
+    # await drone.connect(system_address="serial:///dev/ttyUSB0:57600") 
+    # CAUTION: Color range segmentation is not the best approach to detect and track objects
+    #          This example is just a didactic proof of concept. Don't run it with a real drone
+    #          unless you have good experience flying real drones and know what you are doing.
+    # ----------------------------------------------------------------------------------------- #
+    
+    # Asynchronously poll the connection state until receiving an 'is_connected' confirmation
+    print("Waiting for drone to connect...")
+    async for state in drone.core.connection_state():
+        if state.is_connected:
+            print(f"Drone discovered ")
+            break
+
+    # Activate the drone motors
+    print("-- Arming")
+    await drone.action.arm()
+
+    # Send an initial position to the drone before changing to "offboard" flight mode
+    print("-- Setting initial setpoint")
+    await drone.offboard.set_position_ned(PositionNedYaw(0.0, 0.0, 0.0, 0.0))
+
+    # Change flight mode to "offboard", if it fails, disarm the motors and abort the script
+    print("-- Starting offboard")
+    try:
+        await drone.offboard.start()
+    except OffboardError as error:
+        print(f"Starting offboard mode failed with error code: {error._result.result}")
+        print("-- Disarming")
+        await drone.action.disarm()
+        return
+
+    # Variables to store the NED coordinates, plus Yaw angle. Default pose:
+    N_coord = 0
+    E_coord = 0
+    D_coord = -HOVERING_ALTITUDE # The drone will always detect and track at HOVERING_ALTITUDE
+    yaw_angle = 0 # Drone always points to North
+
+    # Make the drone go (take off) to the default pose
+    await drone.offboard.set_position_ned(
+        PositionNedYaw(N_coord, E_coord, D_coord, yaw_angle))
+    await asyncio.sleep(4) # Give the drone time to gain altitude
+
+    # Infinite detect-follow loop
+    while True:
+        # Get the target coordinates (if any target was detected)
+        #ret, frame = vid_cam.read()
+        tgt_cam_coord, frame = await get_target_coordinates(vid_cam)
+        #print('120')
+        # If a target was found, filter their coordinates
+        if tgt_cam_coord['width'] is not None and tgt_cam_coord['height'] is not None:
+            # Apply Moving Average filter to target camera coordinates
+            tgt_filt_cam_coord = await moving_average_filter(tgt_cam_coord)
+
+        # No target was found, set target camera coordinates to the Cartesian origin,
+        # so the drone doesn't move
+        else:
+            # The Cartesian origin is where the x and y Cartesian axes are located
+            # in the image, in pixel units
+            tgt_cam_coord = {'width':params['y_ax_pos'], 'height':params['x_ax_pos']} # Needed just for drawing objects
+            tgt_filt_cam_coord = {'width':params['y_ax_pos'], 'height':params['x_ax_pos']}
+
+        # Convert from camera coordinates to Cartesian coordinates (in pixel units)
+        tgt_cart_coord = {'x':(tgt_filt_cam_coord['width'] - params['y_ax_pos']),
+                          'y':(params['x_ax_pos'] - tgt_filt_cam_coord['height'])}
+
+        # Compute scaling conversion factor from camera coordinates in pixel units
+        # to Cartesian coordinates in meters
+        COORD_SYS_CONV_FACTOR = 0.1
+
+        # If the target is outside the center rectangle, compute North and East coordinates 
+        if abs(tgt_cart_coord['x']) > params['cent_rect_half_width'] or \
+        abs(tgt_cart_coord['y']) > params['cent_rect_half_height']:
+            # Compute North, East coordinates applying "camera pixel" to Cartesian conversion factor
+            E_coord = tgt_cart_coord['x'] * COORD_SYS_CONV_FACTOR
+            N_coord = tgt_cart_coord['y'] * COORD_SYS_CONV_FACTOR
+            # D_coord, yaw_angle don't change
+
+        # Command the drone to the current NED + Yaw pose
+        await drone.offboard.set_position_ned(
+            PositionNedYaw(N_coord, E_coord, D_coord, yaw_angle))
+
+        # Draw objects over the detection image frame just for visualization
+        frame = await draw_objects(tgt_cam_coord, tgt_filt_cam_coord, frame)
+
+        # Show the detection image frame on screen
+        #print('imshow')
+        cv2.imshow("Detect and Track", frame)
+
+        # Catch aborting key from computer keyboard
+        key = cv2.waitKey(1) & 0xFF
+        # If the 'q' key is pressed, break the 'while' infinite loop
+        if key == ord("q"):
+            break
+    vid_cam.release()
+    cv2.destroyAllWindows()
+   
+    # After leaving the infinite loop, return the drone to home before ending the script
+    print("-- Return to launch...")
+    # await drone.action.return_to_launch()
+    await drone.action.land()
+    print("NOTE: check the drone has landed already before running again this script.")
+    await asyncio.sleep(5) # Wait some time while the drone executes last command
+
+
+async def get_image_params(vid_cam):
+    """ Computes useful general parameters derived from the camera image size."""
+
+    # Grab a frame and get its size
+    is_grabbed, frame = vid_cam.read()
+    params['image_height'], params['image_width'], _ = frame.shape
+
+    # Compute the scaling factor to scale the image to a desired size
+    if params['image_height'] != DESIRED_IMAGE_HEIGHT:
+        params['scaling_factor'] = round((DESIRED_IMAGE_HEIGHT / params['image_height']), 2) # Rounded scaling factor
+        
+    else:
+        params['scaling_factor'] = 1
+
+    print("params['scaling_factor']: ", params['scaling_factor'])
+
+    # Compute resized width and height and resize the image
+    params['resized_width'] = int(params['image_width'] * params['scaling_factor'])
+    params['resized_height'] = int(params['image_height'] * params['scaling_factor'])
+    dimension = (params['resized_width'], params['resized_height'])
+    frame = cv2.resize(frame, dimension, interpolation = cv2.INTER_AREA)
+
+    # Compute the center rectangle's half width and half height
+    params['cent_rect_half_width'] = round(params['resized_width'] * (0.5 * PERCENT_CENTER_RECT)) # Use half percent (0.5)
+    params['cent_rect_half_height'] = round(params['resized_height'] * (0.5 * PERCENT_CENTER_RECT)) # Use half percent (0.5)
+
+    # Compute the minimum target radius to follow. Smaller detected targets will be ignored
+    params['min_tgt_radius'] = round(params['resized_width'] * PERCENT_TARGET_RADIUS)
+
+    # Compute the position for the X and Y Cartesian coordinates in camera pixel units
+    params['x_ax_pos'] = int(params['resized_height']/2 - 1)
+    params['y_ax_pos'] = int(params['resized_width']/2 - 1)
+
+    # Compute two points: p1 in the upper left and p2 in the lower right that will be used to
+    # draw the center rectangle iin the image frame
+    params['cent_rect_p1'] = (params['y_ax_pos'] - params['cent_rect_half_width'], 
+                              params['x_ax_pos'] - params['cent_rect_half_height'])
+    params['cent_rect_p2'] = (params['y_ax_pos'] + params['cent_rect_half_width'], 
+                              params['x_ax_pos'] + params['cent_rect_half_height'])
+
+    return
+
+
+async def get_target_coordinates(vid_cam):
+    """ Detects a target by using color range segmentation and returns its 'camera pixel' coordinates."""
+
+    # Use the 'threshold_inRange.py' script included with the code to get
+    # your own bounds with any color
+    # To detect a blue target:
+    HSV_LOWER_BOUND = (107, 119, 41)
+    HSV_UPPER_BOUND = (124, 255, 255)
+
+    # Grab a frame in BGR (Blue, Green, Red) space color
+    is_grabbed, frame = vid_cam.read()
+
+    # Resize the image frame for the detection process, if needed
+   
+    
+    centerxy= test.det(frame)
+    if len(centerxy) > 0: #en az 1 nesne detect edildiğinde
+        cX, cY = centerxy[0]
+        #print("info")
+        # Return centroid coordinates (camera pixel units), the analized frame and the largest contour
+        return {'width':cX, 'height':cY}, frame
+    else:
+        print('anlamlı')
+        return {'width':None, 'height':None}, frame
+
+
+async def moving_average_filter(coord):
+    """ Applies Low-Pass Moving Average Filter to a pair of (x, y) coordinates."""
+
+    # Append new coordinates to filter buffers
+    filt_buffer['width'].append(coord['width'])
+    filt_buffer['height'].append(coord['height'])
+
+    # If the filters were full already with a number of NUM_FILT_POINTS values, 
+    # discard the oldest value (FIFO buffer)
+    if len(filt_buffer['width']) > NUM_FILT_POINTS:
+        filt_buffer['width'] = filt_buffer['width'][1:]
+        filt_buffer['height'] = filt_buffer['height'][1:]
+    
+    # Compute filtered camera coordinates
+    N = len(filt_buffer['width']) # Get the number of values in buffers (will be < NUM_FILT_POINTS at the start)
+
+    # Sum all values for each coordinate
+    w_sum = sum( filt_buffer['width'] )
+    h_sum = sum( filt_buffer['height'] )
+    # Compute the average
+    w_filt = int(round(w_sum / N))
+    h_filt = int(round(h_sum / N))
+
+    # Return filtered coordinates as a dictionary
+    return {'width':w_filt, 'height':h_filt}
+
+
+async def draw_objects(cam_coord, filt_cam_coord, frame):
+    """ Draws visualization objects from the detection process.
+    Position coordinates of every object are always in 'camera pixel' units"""
+
+    # Draw the Cartesian axes
+    cv2.line(frame, (0, params['x_ax_pos']), (params['resized_width'], params['x_ax_pos']), (0, 128, 255), 1)
+    cv2.line(frame, (params['y_ax_pos'], 0), (params['y_ax_pos'], params['resized_height']), (0, 128, 255), 1)
+    cv2.circle(frame, (params['y_ax_pos'], params['x_ax_pos']), 1, (255, 255, 255), -1)
+    
+    # Draw the center (tolerance) rectangle
+    cv2.rectangle(frame, params['cent_rect_p1'], params['cent_rect_p2'], (0, 178, 255), 1)
+
+    # Draw the detected object's contour, if any
+   
+    # Compute Cartesian coordinates of unfiltered detected object's centroid
+    #x_cart_coord = cam_coord['width'] - params['y_ax_pos']
+    x_cart_coord = cam_coord['width'] 
+
+    #y_cart_coord = params['x_ax_pos'] - cam_coord['height']
+    y_cart_coord = cam_coord['height']
+
+    # Compute Cartesian coordinates of filtered detected object's centroid
+    x_filt_cart_coord = filt_cam_coord['width'] - params['y_ax_pos']
+    y_filt_cart_coord = params['x_ax_pos'] - filt_cam_coord['height']
+
+    # Draw unfiltered centroid as a red dot, including coordinate values
+    cv2.circle(frame, (cam_coord['width'], cam_coord['height']), 5, (0, 0, 255), -1) 
+    cv2.putText(frame, str(x_cart_coord) + ", " + str(y_cart_coord), 
+        (cam_coord['width'] + 25, cam_coord['height'] - 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+
+    # Draw filtered centroid as a blue dot, including coordinate values
+    cv2.circle(frame, (filt_cam_coord['width'], filt_cam_coord['height']), 5, (255, 30, 30), -1)
+    cv2.putText(frame, str(x_filt_cart_coord) + ", " + str(y_filt_cart_coord), 
+        (filt_cam_coord['width'] + 25, filt_cam_coord['height'] - 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 30, 30), 1)
+
+
+    return frame # Return the image frame with all drawn objects
+
+
+if __name__ == "__main__":
+   
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run())
+    
